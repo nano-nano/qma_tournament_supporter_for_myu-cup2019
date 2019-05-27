73b3a6c48e164d9270aa77900d8075bb3ce5770a
Nano-Nano <nano2.aloerina@gmail.com>
2019-05-27 22:01:16 +0900

    Japan Tourのシステム変更に伴う新名称対応

--- src/components/settings/ScoreImportSettingsDialog.vue
+++ src/components/settings/ScoreImportSettingsDialog.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-modal ref="modal" size="lg" title="試合結果取得設定" ok-title="保存して閉じる" cancel-title="保存しないで閉じる" @ok="onOkClicked()">
-      <b-form-group description="大会の試合結果が掲載されるQMA Japan Tour公式サイトのURLを入力します。" label="試合結果掲載ページのURL">
+      <b-form-group description="大会の試合結果が掲載される「QMA 全国魔法学校賢技選手権」公式サイトのURLを入力します。" label="試合結果掲載ページのURL">
         <b-form-input v-model="importUrl" trim></b-form-input>
       </b-form-group>
       <b-form-group description="成績自動追尾機能有効時の成績データ取得間隔を指定します。" label="成績自動追尾間隔（秒）">
